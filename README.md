a1edc75c230a506b60a19bfa47eacb504536db72
Hà Văn Đức <duchv@newwave.vn>
2022-06-30 14:37:19 +0700

    Add l10n

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ A new Flutter project.
 
 ## Getting Started
 
+
+# using with fvm (flutter version management)
 ```shell
 fvm flutter pub run build_runner build
+fvm flutter pub run intl_utils:generate
 ```
--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:get/get.dart';
+import 'package:nws_chat/generated/l10n.dart';
 
 import 'flavors.dart';
 import 'pages/my_home_page.dart';
@@ -7,8 +10,19 @@ import 'pages/my_home_page.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: F.title,
+      localizationsDelegates: const [
+        S.delegate, // Add this line
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', ''), // English, no country code
+      ],
+      locale: const Locale(
+          'en', ''), // TODO: Change this for change default language
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -22,20 +36,20 @@ class App extends StatelessWidget {
   Widget _flavorBanner({
     required Widget child,
     bool show = true,
-  }) =>
-      show
-          ? Banner(
-        child: child,
+  }) {
+    if (show) {
+      return Banner(
         location: BannerLocation.topStart,
         message: F.name,
         color: Colors.green.withOpacity(0.6),
-        textStyle: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 12.0,
-            letterSpacing: 1.0),
+        textStyle: const TextStyle(
+            fontWeight: FontWeight.w700, fontSize: 12.0, letterSpacing: 1.0),
         textDirection: TextDirection.ltr,
-      )
-          : Container(
         child: child,
       );
+    }
+    return Container(
+      child: child,
+    );
+  }
 }
